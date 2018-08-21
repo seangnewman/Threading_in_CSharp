c5e18f88c527053ad27668016780fe4ad63446d2
Sean Newman <seangnewman@msn.com>
2018-08-21 15:13:00 -0700

    Context Switching

--- ContextSwitching/Program.cs
+++ ContextSwitching/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ContextSwitching
@@ -10,6 +11,26 @@ namespace ContextSwitching
     {
         static void Main(string[] args)
         {
+            Thread thread = new Thread(WriteUsingNewThread);
+
+            // worker thread
+            thread.Start();
+
+            //Main thread
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" A" + i + " ");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void WriteUsingNewThread()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" Z" + i + " ");
+            }
         }
     }
 }
